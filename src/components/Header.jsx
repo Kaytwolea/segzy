7c91366cbb312d703dfd8237135db4ad86fff885
Temiloluwa Kolapo <paulkolapo8@gmail.com>
2023-01-28 08:50:12 +0100

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className='w-full bg-white flex justify-between items-center h-16 px-10'>
+        <div className=''>
+          <h2 className='text-black font-bold text-xl '>
+            Segzy Communications(logo)
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
